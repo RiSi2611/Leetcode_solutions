7094b93b39b9c8265c2ff9c88d04c393e53e63e8
Rishikesh Kumar <283556017+RiSi2611@users.noreply.github.com>
2026-08-21 18:11:37 +0530

    Time: 0 ms (100%), Space: 8.3 MB (98.22%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     int reverse(int x) {
-        int remain =0;
-        long long reverse = 0;
-        if(x >= -9 && x <= 9) return x;
-        if (x == INT_MIN) return 0;
-        if(x <0){
+        int remain = 0;
+        int reverse = 0;
+        if (x >= -9 && x <= 9)
+            return x;
+        if (x == INT_MIN)
+            return 0;
+        if (x < 0) {
             x = abs(x);
-            while(x>0){
-                remain = x%10;
-                if(reverse *10 > INT_MAX) return 0;
-                else{
-                    reverse = reverse *10 + remain;
-                    x = x/10;
+            while (x > 0) {
+                remain = x % 10;
+                if (reverse > INT_MAX / 10 ||
+                    (reverse == INT_MAX / 10 && remain > 7))
+                    return 0;
+                else {
+                    reverse = reverse * 10 + remain;
+                    x = x / 10;
                 }
             }
-            if(reverse > (INT_MAX)){
+            if (reverse > (INT_MAX)) {
                 return 0;
-            } 
-            else return (-1)*reverse;
-        }
-        else {
-            while(x>0){
-                remain = x%10;
-                reverse = reverse *10 + remain;
-                x = x/10;
+            } else
+                return (-1) * reverse;
+        } else {
+            while (x > 0) {
+                remain = x % 10;
+                if (reverse > INT_MAX / 10 ||
+                    (reverse == INT_MAX / 10 && remain > 7))
+                    return 0;
+                reverse = reverse * 10 + remain;
+                x = x / 10;
             }
-            if(reverse > INT_MAX){
-                return 0;
-            }
-            else return reverse;
+
+            return reverse;
         }
     }
 };
