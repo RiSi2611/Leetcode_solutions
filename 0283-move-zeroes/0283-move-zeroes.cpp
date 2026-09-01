46e74c8ba02051a19e6206d6abc37c8c2223e22b
Rishikesh Kumar <283556017+RiSi2611@users.noreply.github.com>
2026-09-02 04:03:13 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n= nums.size();
-        int pivot;
-        for(int i=n-2;i>=0 ;i--){
-            if(nums[i]==0){
-                pivot =i;
-                while(pivot <n-1){
-                    swap(nums[pivot],nums[pivot+1]);
-                    pivot++;
-                }
+        int pivot=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[pivot]);
+                pivot++;
             }
-        }
+         }
+        
     }
 };
